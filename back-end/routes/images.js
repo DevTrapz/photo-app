4a768a2e37c0a72783cd1771013b80787867ee81
Andrew Apodaca <andy.apod@gmail.com>
2024-08-05 22:25:48 -0700

    Adds a images route using a mouted host volume

---
+++ back-end/routes/images.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs')
+const path = require('path')
+require('dotenv').config()
+
+const directoryPath = path.join(__dirname, process.env.IMAGE_DATA_DIR);
+
+function getImages() {
+  return new Promise((resolve, reject) => {
+    fs.readdir(directoryPath, (err, files) => {
+      if (err) {
+        reject([]);
+        return;
+      }
+      
+      const images = files.filter(file => file.endsWith('.jpg'));
+      resolve(images);
+    });
+  });
+}
+
+router.get('/', async function(req, res, next) {
+  try {
+    const images = await getImages()
+    res.send(images)
+  } catch (err) {
+    res.send(err)
+  }
+});
+
+module.exports = router;
