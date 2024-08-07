8200dc1ef9014452d4c6bed84c6b12f92957f815
Andrew Apodaca <andy.apod@gmail.com>
2024-08-07 10:48:47 -0700

    adds prettier formatting

--- back-end/app.js
+++ back-end/app.js
@@ -1,29 +1,32 @@
-var express = require('express');
-var path = require('path');
-const cors = require('cors');
-var imageRouter = require('./routes/images');
+var express = require("express");
+var path = require("path");
+const cors = require("cors");
+
+var imageRouter = require("./routes/images");
 
 var app = express();
-app.use(express.json());
+
 app.use(cors());
-app.use('/images', imageRouter);
+app.use("/images", imageRouter);
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
+
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
-const port = 3000
+const port = 3000;
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
-})
+  console.log(`listening on port ${port}`);
+});
 
 module.exports = app;
