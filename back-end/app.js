7cead8bbbcba7ac302b87a129a63276f0c23caed
Andrew Apodaca <andy.apod@gmail.com>
2024-08-07 17:33:45 -0700

    adds /videos route

--- back-end/app.js
+++ back-end/app.js
@@ -3,11 +3,13 @@ var path = require("path");
 const cors = require("cors");
 
 var imageRouter = require("./routes/images");
+var videoRouter = require("./routes/videos");
 
 var app = express();
 
 app.use(cors());
 app.use("/images", imageRouter);
+app.use("/videos", videoRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
