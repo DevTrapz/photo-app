5a5f9262633490d2728cc5be31ea9ac01fd398d2
Andrew Apodaca <andy.apod@gmail.com>
2024-08-08 13:31:38 -0700

    adds Video component to app  and connecting to back-end endpoint

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,31 +4,46 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [images, setImages] = useState([]);
+  const [videos, setVideos] = useState([]);
 
-  const fetchImages = async () => {
+  const fetchData = async () => {
     try {
       const requestOptions = {
         method: "GET",
         mode: "cors" as RequestMode,
       };
 
-      var res = await fetch(
+      const videoRes = await fetch(
+        `http://${import.meta.env.VITE_LOCAL_IPv4}:3000/videos`,
+        requestOptions
+      );
+      const videoData = await videoRes.json();
+      setVideos(videoData);
+
+      const imageRes = await fetch(
         `http://${import.meta.env.VITE_LOCAL_IPv4}:3000/images`,
         requestOptions
       );
-      var data = await res.json();
-      setImages(data);
+      const imageData = await imageRes.json();
+      setImages(imageData);
     } catch (err) {
       console.log(err);
     }
   };
 
   useEffect(() => {
-    fetchImages();
+    fetchData();
   }, []);
 
   return (
     <>
+      {videos[0] &&
+        videos.map((video, index) => (
+          <Video
+            key={index}
+            src={`${import.meta.env.VITE_VIDEO_DATA_DIR}${video}`}
+          />
+        ))}
       {images.map((image, index) => (
         <Card
           key={index}
