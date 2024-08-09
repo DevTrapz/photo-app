be4659965ff51663486fbb47a282a4e78f0627d6
Andrew Apodaca <andy.apod@gmail.com>
2024-08-09 10:14:12 -0700

    adds renderBlocks to handle infinite scrolling that load more data as you scroll. Uses Intersection Observer API

--- front-end/src/Stream.tsx
+++ front-end/src/Stream.tsx
@@ -10,6 +10,25 @@ interface DataObject {
 
 function App() {
   const [dataStream, setDataStream] = useState([]); // Entire data stream payload
+  const [renderBlock, setRenderBlock] = useState([]); // data is render in block of 3
+
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
+
+  var renderNextBlock = (entries) => {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        const newBlockSize = renderBlock.length + 3;
+        setRenderBlock(dataStream.slice(0, newBlockSize));
+      }
+    });
+  };
+
+  var observer = new IntersectionObserver(renderNextBlock, options);
+
   const fetchData = async () => {
     try {
       const requestOptions = {
@@ -22,6 +41,7 @@ function App() {
         requestOptions
       );
       const dataStream: DataObject[] = await res.json();
+      if (dataStream.length > 3) setRenderBlock(dataStream.slice(0, 3));
       setDataStream(dataStream);
     } catch (err) {
       console.log(err);
@@ -31,9 +51,19 @@ function App() {
   useEffect(() => {
     fetchData();
   }, []);
+
+  useEffect(() => {
+    if (renderBlock.length >= 3) {
+      const blockSize = renderBlock.length;
+      const observeID = blockSize - 3;
+      const observedElement = document.getElementById(`${observeID}`);
+      observer.observe(observedElement);
+    }
+  }, [renderBlock]);
+
   return (
     <>
-      {dataStream.map((stream, index) => {
+      {renderBlock.map((stream, index) => {
         if (stream.hasOwnProperty("image")) {
           return <Image id={index} src={stream.image} />;
         } else if (stream.hasOwnProperty("video")) {
--- front-end/src/vite-env.d.ts
+++ front-end/src/vite-env.d.ts
@@ -1,6 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_DATA_DIR: string
+  readonly VITE_IMAGE_DATA_DIR: string
+  readonly VITE_VIDEO_DATA_DIR: string
+  readonly VITE_SHORT_DATA_DIR: string
   readonly VITE_LOCAL_IPv4: string
 }
