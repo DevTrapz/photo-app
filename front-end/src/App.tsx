893fb1ca6f2b42d26d38786c94b4403a996fe445
Andrew Apodaca <andy.apod@gmail.com>
2024-08-09 17:31:12 -0700

    adds Layout component for higher level layout and replaces old App component with Stream component

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,33 @@
-// import { Card } from "flowbite-react";
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Video from "./components/Video";
+import Image from "./components/Image";
+
+interface DataObject {
+  video?: string;
+  image?: string;
+  short?: string;
+}
 
 function App() {
-  const [images, setImages] = useState([]);
-  const [videos, setVideos] = useState([]);
+  const [dataStream, setDataStream] = useState([]); // Entire data stream payload
+  const [renderBlock, setRenderBlock] = useState([]); // data is render in block of 3
+
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
+
+  var renderNextBlock = (entries) => {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        const newBlockSize = renderBlock.length + 3;
+        setRenderBlock(dataStream.slice(0, newBlockSize));
+      }
+    });
+  };
+
+  var observer = new IntersectionObserver(renderNextBlock, options);
 
   const fetchData = async () => {
     try {
@@ -14,19 +36,13 @@ function App() {
         mode: "cors" as RequestMode,
       };
 
-      const videoRes = await fetch(
-        `http://${import.meta.env.VITE_LOCAL_IPv4}:3000/videos`,
+      const res = await fetch(
+        `http://${import.meta.env.VITE_LOCAL_IPv4}:3000/stream`,
         requestOptions
       );
-      const videoData = await videoRes.json();
-      setVideos(videoData);
-
-      const imageRes = await fetch(
-        `http://${import.meta.env.VITE_LOCAL_IPv4}:3000/images`,
-        requestOptions
-      );
-      const imageData = await imageRes.json();
-      setImages(imageData);
+      const dataStream: DataObject[] = await res.json();
+      if (dataStream.length > 3) setRenderBlock(dataStream.slice(0, 3));
+      setDataStream(dataStream);
     } catch (err) {
       console.log(err);
     }
@@ -36,22 +52,36 @@ function App() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    if (renderBlock.length >= 3) {
+      const blockSize = renderBlock.length;
+      const observeID = blockSize - 3;
+      const observedElement = document.getElementById(`${observeID}`);
+      observer.observe(observedElement);
+    }
+  }, [renderBlock]);
+
   return (
     <>
-      {videos[0] &&
-        videos.map((video, index) => (
-          <Video
-            key={index}
-            src={`${import.meta.env.VITE_VIDEO_DATA_DIR}${video}`}
-          />
-        ))}
-      {images.map((image, index) => (
-        <img
-          key={index}
-          src={`${import.meta.env.VITE_IMAGE_DATA_DIR}${image}`}
-          className="max-w-md  mx-auto bg-zinc-800 border-0 rounded-none"
-        />
-      ))}
+      {renderBlock.map((stream, index) => {
+        if (stream.hasOwnProperty("image")) {
+          return <Image id={index} src={stream.image} />;
+        } else if (stream.hasOwnProperty("video")) {
+          return (
+            <Video
+              id={index}
+              src={`${import.meta.env.VITE_VIDEO_DATA_DIR}${stream.video}`}
+            />
+          );
+        } else if (stream.hasOwnProperty("short")) {
+          return (
+            <Video
+              id={index}
+              src={`${import.meta.env.VITE_SHORT_DATA_DIR}${stream.short}`}
+            />
+          );
+        }
+      })}
     </>
   );
 }
---
+++ front-end/src/Layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "./App";
+import OptionsMenu from "./components/OptionsMenu";
+
+export default function Layout() {
+  return (
+    <>
+      <OptionsMenu />
+      <App />
+    </>
+  );
+}
