ff14065fa991223dffeab46d393645a3d84e27ab
Andrew Apodaca <andy.apod@gmail.com>
2024-08-11 10:32:13 -0700

    adds required dependencies for react-cookie, and corrections to main.tsx

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Stream from "./Stream.tsx";
+import Layout from "./Layout.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Stream />
+    <Layout />
   </React.StrictMode>
 );
