87d4ba61398780896008bfba487f49638521630b
Andrew Apodaca <andy.apod@gmail.com>
2024-08-13 09:26:20 -0700

    sets cookies on first render if they don't exsit

--- front-end/src/Layout.tsx
+++ front-end/src/Layout.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import App from "./App";
 import OptionsMenu from "./components/OptionsMenu";
-import { CookiesProvider } from "react-cookie";
+import { CookiesProvider, useCookies } from "react-cookie";
 
 export default function Layout() {
+  const [cookies, setCookies] = useCookies(["preferences"]);
+
+  if (!cookies.preferences)
+    setCookies("preferences", {
+      "display-options": ["video", "photo", "short"],
+    });
+
   return (
     <CookiesProvider>
       <OptionsMenu />
