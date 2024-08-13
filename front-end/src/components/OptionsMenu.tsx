f0c1a237eb93af3397fd43e1bab949e620689dc0
Andrew Apodaca <andy.apod@gmail.com>
2024-08-13 09:34:21 -0700

    adds options buttons as a open close drawer

--- front-end/src/components/OptionsMenu.tsx
+++ front-end/src/components/OptionsMenu.tsx
@@ -1,21 +1,11 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useCookies } from "react-cookie";
-
-interface preferences {
-  "display-video"?: boolean;
-  "display-photo"?: boolean;
-  "display-short"?: boolean;
-}
-
+import { Drawer } from "flowbite-react";
+import "./OptionStyles.css";
 
 export default function OptionsMenu() {
   const [cookies, setCookies] = useCookies(["preferences"]);
-  const isOptionsOpen = useRef(false);
-
-  function getDisplayType(element: HTMLElement) {
-    var option = element.className;
-    return option.split(" ")[0];
-  }
+  const [isOpen, setIsOpen] = useState(false);
 
   function toggleOption(e) {
     const btn: HTMLElement = e.currentTarget;
@@ -54,20 +44,66 @@ export default function OptionsMenu() {
     setPreferredOptions();
   }, [cookies.preferences]);
 
+  function handleOpen() {
+    setIsOpen(true);
+  }
+  function handleClose() {
+    setIsOpen(false);
+  }
   return (
-    <div className="options-drawer">
-      <div className="media-options">
-        <h1>Choose Media</h1>
-        <button className="display-video" onClick={toggleOption}>
-          Video
-        </button>
-        <button className="display-photo" onClick={toggleOption}>
-          Photo
-        </button>
-        <button className="display-short" onClick={toggleOption}>
-          Short
+    <>
+      <div className="flex max-w-md w-full mx-auto flex-row-reverse">
+        <button onClick={handleOpen} className="fixed z-10">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-15 text-white my-4;"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z"
+            />
+          </svg>
         </button>
       </div>
-    </div>
+
+      <Drawer
+        open={isOpen}
+        onClose={handleClose}
+        position="top"
+        className="max-w-md w-full mx-auto"
+      >
+        <Drawer.Header title="Choose Media" />
+        <Drawer.Items>
+          <div className="grid grid-cols-1 gap-5 md:grid-cols-3">
+            <button
+              id="video"
+              onClick={toggleOption}
+              className="display-video py-2 text-white hover:bg-cyan-800 rounded-lg"
+            >
+              Video
+            </button>
+            <button
+              id="photo"
+              onClick={toggleOption}
+              className="display-photo py-2 text-white hover:bg-cyan-800 rounded-lg"
+            >
+              Photo
+            </button>
+            <button
+              id="short"
+              onClick={toggleOption}
+              className="display-short py-2 text-white hover:bg-cyan-800 rounded-lg"
+            >
+              Short
+            </button>
+          </div>
+        </Drawer.Items>
+      </Drawer>
+    </>
   );
 }
