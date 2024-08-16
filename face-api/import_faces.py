ffa63cab166dc11f14d3a5e21dee2af73576d314
Andrew Apodaca <andy.apod@gmail.com>
2024-08-15 22:27:55 -0700

    refactors code and adds a method to check db for processed photos before processing more

--- face-api/import_faces.py
+++ face-api/import_faces.py
@@ -4,43 +4,79 @@ import numpy as np
 import psycopg2
 import os
 
-dbIP = os.environ.get('DB_IP')
-dbPort = os.environ.get('DB_PORT')
-dbUser = os.environ.get('POSTGRES_USER')
-dbPassword = os.environ.get('POSTGRES_PASSWORD')
+def dbConnectionSetup():
+    dbIP = os.environ.get('DB_IP')
+    dbPort = os.environ.get('DB_PORT')
+    dbUser = os.environ.get('POSTGRES_USER')
+    dbPassword = os.environ.get('POSTGRES_PASSWORD')
+    dbConnect = psycopg2.connect(f'postgres://{dbUser}:{dbPassword}@{dbIP}:{dbPort}')
+    return dbConnect
 
-db = psycopg2.connect(f'postgres://{dbUser}:{dbPassword}@{dbIP}:{dbPort}')
+def fetchPhotos():
+    api_url = "http://192.168.56.1:3000/images"
+    response = requests.get(api_url)
+    photos = response.json()
+    return photos
 
-api_url = "http://192.168.56.1:3000/images"
-response = requests.get(api_url)
-response = response.json()
+def extractValue(row):
+    return row[0].split("'")[0]
 
-shortList = []
-for x in range(10):
-    shortList.append(response[x])
+def filterProcessedPhotos(dbConnect, photos):
+    db = dbConnect.cursor()
 
-faceCount = 0
-for photo in shortList:
+    db.execute(f"SELECT picture FROM pictures WHERE picture = ANY(ARRAY{photos}) ")
+    rows = db.fetchall()
+    processedPhotos = list(map(extractValue, rows))
+    unprocessedPhotos = [photo for photo in photos if photo not in processedPhotos]
+    db.close()
+    return unprocessedPhotos
+
+def extractFaces(file_name):
     alg = "haarcascade_frontalface_default.xml"
     haar_cascade = cv2.CascadeClassifier(alg)
-    file_name = "/app/data/Reddit/" + photo
     img = cv2.imread(file_name, 0)
     gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     faces = haar_cascade.detectMultiScale(
         gray_img, scaleFactor=1.01, minNeighbors=15, minSize=(150, 150)
     )
-    
-    # Store photos of each face detected
+
+    return faces, img
+
+def storeFaces(faces, img):
+    faceCount = 0
     for x, y, w, h in faces:
-        # crop the image to select only the face
         cropped_image = img[y : y + h, x : x + w]
-        # loading the target image path into target_file_name variable  - replace <INSERT YOUR TARGET IMAGE NAME HERE> with the path to your target image
         target_file_name = 'stored-faces/' + str(faceCount) + '.jpg'
         cv2.imwrite(
             target_file_name,
             cropped_image,
         )
         faceCount = faceCount + 1;
+    return faceCount
 
-    print(f'{file_name:<{65}}  {len(faces)} faces detected')
-print(f'\nFace detection Complete!\n{len(shortList)} images scanned\n{faceCount} faces detected')
+def processPhotos(dbConnect, photos):
+    db = dbConnect.cursor()
+    faceCount = 0
+    for photo in photos:
+        file_name = "/app/data/Reddit/" + photo
+        faces, img = extractFaces(file_name)
+        faceCount = faceCount + storeFaces(faces, img)
+
+
+       
+        
+        # Generate and store embedding
+        img = Image.open(file_name)
+        ibed = imgbeddings()
+        embeddings = ibed.to_embeddings(img)
+        for embedding in embeddings:
+            db.execute("INSERT INTO pictures values (%s,%s)", (photo, embedding.tolist()))
+            dbConnect.commit()
+        print(f'{file_name:<{65}}  {len(faces)} faces detected')
+    db.close()
+    print(f'\nFace detection Complete!\n{len(photos)} images scanned\n{faceCount} faces detected')
+
+dbConnect = dbConnectionSetup()
+photos = fetchPhotos()
+unprocessedPhotos = filterProcessedPhotos(dbConnect, photos[:100])
+processPhotos(dbConnect, unprocessedPhotos)
