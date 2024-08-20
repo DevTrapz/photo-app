07f418ebaa06ede60167d603d42cf5cfcd3bd58a
Andrew Apodaca <andy.apod@gmail.com>
2024-08-19 23:28:26 -0700

    adds auto play to Video component when a visability threshold is exceeded

--- web-mobile-ui/src/components/Video.tsx
+++ web-mobile-ui/src/components/Video.tsx
@@ -1,9 +1,34 @@
+import { useEffect } from "react";
+
 interface props {
   src: string;
   id: number;
 }
 
 export default function Video({ src, id }: props) {
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: [0.2],
+  };
+
+  var playVideo = (entries: IntersectionObserverEntry[]) => {
+    entries.forEach(function (entry) {
+      if (entry.target.buffered.length < 1) return;
+      if (entry.isIntersecting) {
+        entry.target.play();
+      } else {
+        entry.target.pause();
+      }
+    });
+  };
+
+  useEffect(() => {
+    const video = document.getElementById(id.toString());
+    var observer = new IntersectionObserver(playVideo, options);
+    observer.observe(video);
+  }, []);
+
   return (
     <>
       <video
@@ -12,8 +37,9 @@ export default function Video({ src, id }: props) {
         controls
         preload="metadata"
         loop={true}
-        autoPlay={true}
+        autoPlay={false}
         muted={true}
+        playsInline
         className="max-w-md w-full mx-auto mb-1"
       >
         <source src={src} type="video/mp4"></source>
